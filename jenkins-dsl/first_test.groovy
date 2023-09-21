5b96d1e0f9005a8330424bc67e6b1061a3edf582
Ray Wang <wlei@m2.com>
2023-09-21 14:35:04 +0800

    add the first one

---
+++ jenkins-dsl/first_test.groovy
@@ -0,0 +1,74 @@
+pipelineJob("dsl/job-just-test") {
+        environmentVariables(
+            GIT_URL_JOB: "https://github.com/rrrrrrrrrray/vms.git"
+            GIT_URL_CONF: "https://github.com/rrrrrrrrrray/nconf.git"
+        )
+
+        logRotator {
+            numToKeep(3)
+        }
+
+        parameters {
+            gitParameter {
+                name('branch')
+                type('PT_BRANCH_TAG')
+                defaultValue('main')
+                description('')
+                branch('')
+                branchFilter('origin1/(.*)')
+                tagFilter('*')
+                sortMode('NONE')
+                selectedValue('NONE')
+                useRepository($GIT_URL_JOB)
+                quickFilterEnabled(true)
+                listSize('8')
+                requiredParameter(false)
+            }
+        }
+
+        definition {
+            cpsScm {
+                scm {
+                    gitSCM {
+                      userRemoteConfigs {
+                        userRemoteConfig {
+                          url($GIT_URL_JOB)
+                          credentialsId()
+                          name('origin')
+                          refspec('+refs/heads/*:refs/remotes/jenkins-ci/*')
+                          browser {
+                            gitLab {
+                              repoUrl("https://github.com/")
+                              version("15.3")
+                            }
+                          }
+                          gitTool("/opt/homebrew/bin/git")
+                        }
+                        
+                        userRemoteConfig {
+                          url($GIT_URL_JOB)
+                          credentialsId()
+                          name('origin1')
+                          refspec('+refs/heads/*:refs/remotes/sms-gateway/*')
+                          browser {
+                            gitLab {
+                              repoUrl("https://github.com/")
+                              version("15.3")
+                            }
+                          }
+                          gitTool("/opt/homebrew/bin/git")
+                        }
+                      }
+                      branches {
+                        branchSpec {
+                          name("main")
+                          }
+                        }
+                    }
+                }
+
+                scriptPath("JenkinsFiles/none-prod-golang-jenkinsfile")
+
+            }
+        }
+    }
