29ba8d4515784dc1ee86e366810248be43671e7d
Ray Wang <wlei@m2.com>
2023-09-21 23:10:10 +0800

    update

--- jenkins-dsl/first_test.groovy
+++ jenkins-dsl/first_test.groovy
@@ -42,7 +42,6 @@ pipelineJob("dsl/job-just-test") {
                       userRemoteConfigs {
                         userRemoteConfig {
                           url(git_jks_conf)
-                          // credentialsId()
                           name('origin')
                           refspec('+refs/heads/*:refs/remotes/jenkins-ci/*')
                           browser {
@@ -56,7 +55,6 @@ pipelineJob("dsl/job-just-test") {
                         
                         userRemoteConfig {
                           url(git_jks_job)
-                          // credentialsId()
                           name('origin1')
                           refspec('+refs/heads/*:refs/remotes/sms-gateway/*')
                           browser {
